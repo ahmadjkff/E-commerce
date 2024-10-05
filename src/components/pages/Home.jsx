bdb9828ab6c175fc8a32c29ff73513d692c46a75
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2024-10-05 22:44:53 +0300

    Refactor Home page to include a "Move to Top" button

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,16 +8,24 @@ import Explore from "./Home/Explore";
 import New from "./Home/New";
 import Services from "./Home/Services";
 import ad2 from "../../assets/Home/ad2.png";
+import uparrow from "../../assets/Home/uparrow.png";
 
 function Home() {
   const [products, setProducts] = useState([]);
   const [limit, setLimit] = useState(4);
 
+  const moveTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   useEffect(() => {
     ProductsData(limit).then((data) => setProducts(data));
   }, [limit]);
   return (
-    <div className="flex flex-col gap-[140px] mb-[140px] xs:mx-20 md:mx-[135px]">
+    <div className="flex flex-col gap-[140px] mb-[140px] xs:mx-20 md:mx-[135px] relative">
       <FirstSection />
       <FlashSales products={products} />
       <BrowseByCategory />
@@ -26,6 +34,12 @@ function Home() {
       <Explore products={products} limit={limit} setLimit={setLimit} />
       <New />
       <Services />
+      <button
+        className="absolute -bottom-28 right-0 p-3 rounded-full bg-secondary"
+        onClick={moveTop}
+      >
+        <img src={uparrow} alt="" width={24} />
+      </button>
     </div>
   );
 }
