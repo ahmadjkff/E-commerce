c76a7fb8c86fa3b641df47f8ed75d06b5ee0c328
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2024-10-05 22:53:34 +0300

    Refactor About page to update icons and styling

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,8 +8,10 @@ import emma from "../../assets/emma.png";
 import twitterblack from "../../assets/twitterblack.png";
 import linkedinblack from "../../assets/linkedinblack.png";
 import instagramblack from "../../assets/instagramblack.png";
-import headphones from "../../assets/headphones.png";
 import dots from "../../assets/dots.png";
+import truck from "../../assets/Home/truck.png";
+import headphone from "../../assets/Home/headphone.png";
+import guarantee from "../../assets/Home/guarantee.png";
 function About() {
   return (
     <div>
@@ -135,27 +137,31 @@ function About() {
         <img className="w-[112px] h-4" src={dots} alt="dots" />
       </div>
 
-      <div className="flex flex-col sm:flex-col md:flex-row gap-[88px] mx-[248px] my-[140px]">
+      <div className="flex justify-around my-[140px] xs:flex-col xs:gap-8 md:flex-row">
         <div className="flex flex-col items-center">
-          <img className="rounded-full w-20" src={truck} alt="truck icon" />
+          <div className="bg-customblack rounded-full p-2">
+            <div className="bg-black p-2 rounded-full">
+              <img className=" w-10" src={truck} alt="truck icon" />
+            </div>
+          </div>
           <p className="font-semibold text-xl mt-6">FREE AND FAST DELIVERY</p>
           <p className="mt-2">Free delivery for all orders over $140</p>
         </div>
         <div className="flex flex-col items-center">
-          <img
-            className="rounded-full w-20"
-            src={headphones}
-            alt="headphone icon"
-          />
+          <div className="bg-customblack rounded-full p-2">
+            <div className="bg-black p-2 rounded-full">
+              <img className=" w-10" src={headphone} alt="headphone icon" />
+            </div>
+          </div>
           <p className="font-semibold text-xl mt-6">24/7 CUSTOMER SERVICE</p>
           <p className="mt-2">Friendly 24/7 customer support</p>
         </div>
         <div className="flex flex-col items-center">
-          <img
-            className="rounded-full w-20"
-            src={guarantee}
-            alt="guarantee icon"
-          />
+          <div className="bg-customblack rounded-full p-2">
+            <div className="bg-black p-2 rounded-full">
+              <img className=" w-10" src={guarantee} alt="guarantee icon" />
+            </div>
+          </div>
           <p className="font-semibold text-xl mt-6">MONEY BACK GUARANTEE</p>
           <p className="mt-2">We reurn money within 30 days</p>
         </div>
