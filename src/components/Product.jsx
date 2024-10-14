b586d61da32e446f04ff768c107cfa7b01ffe97b
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2024-10-14 22:06:06 +0300

    Refactor SVG stroke attributes to adhere to naming conventions

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -52,9 +52,9 @@ function Product({ rating, ratingReadonly, isWishList, product }) {
             <path
               d="M6 1C3.239 1 1 3.216 1 5.95C1 8.157 1.875 13.395 10.488 18.69C10.6423 18.7839 10.8194 18.8335 11 18.8335C11.1806 18.8335 11.3577 18.7839 11.512 18.69C20.125 13.395 21 8.157 21 5.95C21 3.216 18.761 1 16 1C13.239 1 11 4 11 4C11 4 8.761 1 6 1Z"
               stroke="black"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </button>
@@ -73,16 +73,16 @@ function Product({ rating, ratingReadonly, isWishList, product }) {
             <path
               d="M20.257 6.962C20.731 7.582 20.731 8.419 20.257 9.038C18.764 10.987 15.182 15 11 15C6.81801 15 3.23601 10.987 1.74301 9.038C1.51239 8.74113 1.38721 8.37592 1.38721 8C1.38721 7.62408 1.51239 7.25887 1.74301 6.962C3.23601 5.013 6.81801 1 11 1C15.182 1 18.764 5.013 20.257 6.962V6.962Z"
               stroke="black"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <path
               d="M11 11C12.6569 11 14 9.65685 14 8C14 6.34315 12.6569 5 11 5C9.34315 5 8 6.34315 8 8C8 9.65685 9.34315 11 11 11Z"
               stroke="black"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </Link>
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -109,7 +109,7 @@ export default function Menu() {
                             className="flex gap-2"
                           >
                             <svg
-                              strokeWidth="32"
+                              width="32"
                               height="32"
                               viewBox="0 0 32 32"
                               fill="none"
@@ -147,22 +147,22 @@ export default function Menu() {
                               <path
                                 d="M3 6.3V20.5C3 20.7652 3.10536 21.0196 3.29289 21.2071C3.48043 21.3946 3.73478 21.5 4 21.5H20C20.2652 21.5 20.5196 21.3946 20.7071 21.2071C20.8946 21.0196 21 20.7652 21 20.5V6.3H3Z"
                                 stroke="#FAFAFA"
-                                stroke-width="1.5"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeLinejoin="round"
                               />
                               <path
                                 d="M21 6.3L18.1665 2.5H5.8335L3 6.3M15.7775 9.6C15.7775 11.699 14.0865 13.4 12 13.4C9.9135 13.4 8.222 11.699 8.222 9.6"
                                 stroke="#FAFAFA"
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                               />
                             </svg>
 
-                            <span>My Order</span>
+                            <Link to={"/orders"}>My Order</Link>
                           </MenuItem>,
                           <MenuItem
-                            key="reviews"
+                            key="Cancellations"
                             onClick={handleClose}
                             className="flex gap-2"
                           >
@@ -173,20 +173,20 @@ export default function Menu() {
                               fill="none"
                               xmlns="http://www.w3.org/2000/svg"
                             >
-                              <g clip-path="url(#clip0_9507_978)">
+                              <g clipPath="url(#clip0_9507_978)">
                                 <path
                                   d="M8 16L12 12M16 8L11.9992 12M11.9992 12L8 8M12 12L16 16"
                                   stroke="#FAFAFA"
-                                  stroke-width="1.5"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
+                                  strokeWidth="1.5"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                                 />
                                 <circle
                                   cx="12"
                                   cy="12"
                                   r="11.25"
                                   stroke="white"
-                                  stroke-width="1.5"
+                                  strokeWidth="1.5"
                                 />
                               </g>
                               <defs>
@@ -213,7 +213,7 @@ export default function Menu() {
                               <path
                                 d="M19.8284 9.93621C20.4517 9.93621 20.7176 10.7286 20.2205 11.1046L16.8905 13.6234C16.1688 14.1693 15.8661 15.1087 16.1334 15.9732L17.3864 20.0261C17.5735 20.6312 16.8729 21.1193 16.3701 20.7341L13.3075 18.3879C12.536 17.7969 11.464 17.7969 10.6925 18.3879L7.61357 20.7466C7.11152 21.1312 6.41161 20.645 6.59677 20.0403L7.83243 16.0046C8.09532 15.146 7.79694 14.2145 7.08413 13.6684L3.73432 11.1022C3.24111 10.7244 3.50831 9.93621 4.12961 9.93621H8.12744C9.07024 9.93621 9.90305 9.32198 10.1815 8.42125L11.379 4.5479C11.5678 3.93721 12.4322 3.93722 12.621 4.5479L13.8185 8.42124C14.0969 9.32198 14.9298 9.93621 15.8726 9.93621H19.8284Z"
                                 stroke="#FAFAFA"
-                                stroke-width="1.5"
+                                strokeWidth="1.5"
                               />
                             </svg>
 
@@ -234,9 +234,9 @@ export default function Menu() {
                               <path
                                 d="M4 12H13.5M6 15L3 12L6 9M11 7V6C11 5.46957 11.2107 4.96086 11.5858 4.58579C11.9609 4.21071 12.4696 4 13 4H18C18.5304 4 19.0391 4.21071 19.4142 4.58579C19.7893 4.96086 20 5.46957 20 6V18C20 18.5304 19.7893 19.0391 19.4142 19.4142C19.0391 19.7893 18.5304 20 18 20H13C12.4696 20 11.9609 19.7893 11.5858 19.4142C11.2107 19.0391 11 18.5304 11 18V17"
                                 stroke="#FAFAFA"
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                               />
                             </svg>
 
